af101ee75197b620cfd5b4342514203f348166e3
Cade Sperlich <csperlich@gmail.com>
2016-02-26 22:52:41 -0500

    add skeleton files, index.ejs is loading

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    morgan = require('morgan'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    config = require('./config/config'),
+    methodOverride = require('method-override'),
+    port = config.defaultPort || 8080;
+
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+if (config.logging) {
+  app.use(morgan('dev'));
+}
+
+require('./app/routes/common.routes')(app);
+
+app.use(express.static('./public'));
+/*
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('common/pages/404', {url: req.url});
+});
+*/
+app.listen(port);
+console.log('listening on port ' + port);
