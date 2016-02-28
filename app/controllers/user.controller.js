9cb834d8116bd2b25604826db7cb4b893aa3cf30
Cade Sperlich <csperlich@gmail.com>
2016-02-28 03:08:14 -0500

    add confirmation number creation, register and unregister creates the confirmation number and likewise removes it along with the registration.

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,22 +2,29 @@ var User = require('mongoose').model('User');
 
 exports.create = function(req, res, next) {
   console.log('in create');
-	var user = new User(req.body);
-	user.save(function(err, user) {
-		if (err) {
-			return next(err);
-		} else {
+  var user = new User(req.body);
+  user.save(function(err, user) {
+    if (err) {
+      return next(err);
+    } else {
       req.user = user;
       next();
-		}
-	});
+    }
+  });
 };
 
 exports.register = function(req, res, next) {
   console.log('in register');
   User.update(
     { _id: req.user._id },
-    { $addToSet: { registrations: req.conference._id } },
+    {
+      $addToSet: {
+        registrations: {
+          registrationID: req.conference._id,
+          confirmationNumber: User.getNextConfirmationNumber()
+        }
+      }
+    },
     function(err) {
       if (err) {
         return next(err);
@@ -32,7 +39,13 @@ exports.unRegister = function(req, res, next) {
   console.log('in unregister');
   User.update(
     { _id: req.user._id },
-    { $pull: { registrations: req.conference._id } },
+    {
+      $pull: {
+        registrations: {
+          registrationID: req.conference._id
+        }
+      }
+    },
     function(err) {
       if (err) {
         return next(err);
@@ -45,14 +58,14 @@ exports.unRegister = function(req, res, next) {
 
 exports.userByEmail = function(req, res, next) {
   console.log('in userByEmail');
-	User.findOne({
-		email: req.query.email || req.body.email
-	}, function(err, user) {
-		if (err) {
-			return next(err);
-		} else {
-			req.user = user;
-			next();
-		}
-	});
+  User.findOne({
+    email: req.query.email || req.body.email
+  }, function(err, user) {
+    if (err) {
+      return next(err);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
 };
--- app/models/users/user.model.js
+++ app/models/users/user.model.js
@@ -31,8 +31,33 @@ var UserSchema = new Schema({
 		default: Date.now
 	},
 	registrations: [{
-      registrationID: Schema.Types.ObjectId
+      registrationID: Schema.Types.ObjectId,
+			confirmationNumber: {
+				type: String,
+				index: true
+			}
   }]
 });
 
+UserSchema.statics.chars = 'ABCDEFGHIJKLMNPQRSTUVWXYZ';
+UserSchema.statics.getNextConfirmationNumber = function() {
+	var letters = [];
+	for (var i = 0; i < 8; i++) {
+		letters.push(UserSchema.statics.chars[Math.floor(Math.random() * UserSchema.statics.chars.length)]);
+	}
+
+	//unix time millisecons, of length 13 for the foreseeable future
+	var numbers = new Date().getTime().toString().split('');
+	var output = letters.concat(numbers);
+
+    var i = output.length;
+    while( i > 1) {
+    	i = i - 1;
+        var j = Math.floor(Math.random() * i);
+        var temp = output[j];
+        output[j] = output[i];
+        output[i] = temp;
+	}
+	return output.join('');
+};
 mongoose.model('User', UserSchema);
